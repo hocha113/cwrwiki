3e2632c45863648d54cf4cbe3a7783c4fc4e5af6
hocha <2188704314@qq.com>
2024-12-25 17:34:11 +0800

    46

--- index.js
+++ index.js
@@ -14,30 +14,48 @@ categories.forEach(function(category) {
     });
 });
 
-//愚蠢的声音设计
-// // 为每个 .category 元素添加鼠标进入事件监听器
-// categories.forEach(category => {
-//     category.addEventListener('mouseenter', function() {
-//         // 防止音效快速重复播放
-//         if (!isSoundPlaying) {
-//             isSoundPlaying = true;
-//             soundEffect.currentTime = 0;  // 重置播放时间
-//             soundEffect.play();
-//         }
-//     });
-
-//     // 当鼠标离开时，重置 isSoundPlaying 标记
-//     category.addEventListener('mouseleave', function() {
-//         isSoundPlaying = false;
-//     });
-// });
-
 function HanderCategoryEvent(id){
     // 这个函数用于处理主页窗口的调整逻辑，注意id必须是一个可以转换为数字的值
     const currentLang = localStorage.getItem('selectedLanguage') || "zh-CN";
     switch(Number(id)){
         case 1:
             window.location.href = currentLang === 'zh-CN' ?  "cn/core/weaponList.html" : "en/core/weaponList.html";
+            break;
+        case 2:
+            window.location.href = currentLang === 'zh-CN' ?  "cn/core/mechanism.html" : "en/core/mechanism.html";
+            break;
+        case 3:
+            window.location.href = currentLang === 'zh-CN' ?  "cn/core/biology.html" : "en/core/biology.html";
+            break;
+    }
+}
+
+// 获取所有的 <li> 元素
+const sidebarItems = document.querySelectorAll('.sidebar ul li');
+
+// 为每个 li 元素添加点击事件
+sidebarItems.forEach(function(item) {
+    item.addEventListener('click', function() {
+        const id = item.id; // 获取当前 li 的 id
+        handleSidebarNavigation(id);
+    });
+});
+
+function handleSidebarNavigation(id) {
+    // 获取当前语言选项
+    const currentLang = localStorage.getItem('selectedLanguage') || "zh-CN";
+    switch (id) {
+        case "1s":
+            window.location.href = currentLang === 'zh-CN' ? "cn/core/weaponList.html" : "en/core/weaponList.html";
+            break;
+        case "2s":
+            window.location.href = currentLang === 'zh-CN' ? "cn/core/mechanism.html" : "en/core/mechanism.html";
+            break;
+        case "3s":
+            window.location.href = currentLang === 'zh-CN' ? "cn/core/biology.html" : "en/core/biology.html";
+            break;
+        case "4s":
+            window.location.href = currentLang === 'zh-CN' ? "cn/core/aboutMod.html" : "en/core/aboutMod.html";
             break;
     }
 }
